db708fe1f5967f1e6b5da00302aeebf75e56b609
David Lundholm <david.lundholm@zocom.se>
2021-06-21 10:25:33 +0200

    Clean up

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
-
 const [,, ...args] = process.argv
 const { exec } = require('child_process')
 const path = require('path')
 
-// process.argv.push("modules/8.authorization/test.md")
 process.argv.push("--preprocessor",  path.join(__dirname,"preprocessor","commands.js") )
 process.argv.push("--highlight-theme", "monokai" )
-process.argv.push("--theme",  path.join("node_modules", "@zocom", "zocom-presenter","assets","styles","theme.css"))
+process.argv.push("--theme",  path.join("node_modules", "@zocom", "presenter","assets","styles","theme.css"))
 
 console.log("-- Zocom Presenter --")
 require('./node_modules/reveal-md/bin/reveal-md.js')
