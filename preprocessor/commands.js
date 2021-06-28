e0d2a81fab654545d76079e46d76d4d3346668d3
David Lundholm <david.lundholm@zocom.se>
2021-06-28 14:44:43 +0200

    Add two column layout directive and styles

--- preprocessor/commands.js
+++ preprocessor/commands.js
@@ -2,6 +2,7 @@ const COMMAND_CHARACTERS = "::"
 
 module.exports = (markdown, options) => {
     return new Promise((resolve, reject) => {
+      let inColumn = false
       return resolve(
         markdown
           .split('\n')
@@ -11,12 +12,21 @@ module.exports = (markdown, options) => {
             const instruction = instructionLine[1]
             if(instruction.startsWith("bg-")){
                 return `<!-- .slide: class="${instruction.replace("bg-","")}"-->`
+            }else if(instruction.startsWith("layout-")){
+                return `<!-- .slide: class="${instruction.replace("layout-","")}"-->`
+            }else if(instruction == 'column'){
+              let result = inColumn ? "</div><div>\n" : '<div>\n'
+              inColumn = true
+              return result
             }else if(instruction == "fragment"){
                 return line.replace("::fragment", "<!-- .element: class=\"fragment\" -->")
+            }else if(inColumn && line.startsWith("---")){
+              inColumn = false
+              return '</div>\n' + line + '\n'
             }else{
                 return line
             }
-          })
+          })          
           .join('\n')
       );
     });
