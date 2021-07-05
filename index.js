cc14d866ddf5815bc57bab9953f84d38478ec132
David Lundholm <david.lundholm@zocom.se>
2021-07-05 15:01:06 +0200

    Add reveal config copying and cleanup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const [,, ...args] = process.argv
 const { exec } = require('child_process')
 const path = require('path')
+const fs = require('fs')
 
 const PRESENTER_PATH = path.join('node_modules', '@zocom', 'presenter')
 
@@ -16,5 +17,18 @@ if(!args.includes("--theme")){
   process.argv.push("--theme",  'https://zocom-utbildning.github.io/zocom-presenter-theme/zocom.css' )
 }
 
+function cleanup(){
+  if(fs.existsSync(revealJSONPath))
+    (fs.unlinkSync(path.join(process.cwd(), 'reveal.json')))
+}
+const revealJSONPath = path.join(process.cwd(), 'reveal.json')
+
+if(!fs.existsSync(revealJSONPath)){
+  fs.copyFileSync(path.join(__dirname, './reveal.json'), revealJSONPath)
+  process.on('exit', cleanup )
+  process.on('SIGINT', cleanup )
+}
+
 console.log("-- Zocom Presenter --")
+
 require('reveal-md/bin/reveal-md')
